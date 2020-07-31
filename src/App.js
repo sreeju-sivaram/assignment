558f272f804c950f3d220039ce181384a50d1e1c
unknown <sreejusivaram47@gmail.com>
2020-07-31 23:55:16 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PrimarySearchAppBar from './Navbar';
+import MediaCard from './Card';
+import { Paper } from '@material-ui/core';
+import Image from './sky.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  paperContainer: {
+      backgroundImage: `url(${Image})`,
+      height: "100vh",
+  }
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: 'London'
+    }
+  }
+
+  setCity = (place) => {
+    this.setState({
+      city: place
+    })
+  }
+  render () {
+    return (
+      <div className="App">
+        <Paper style={styles.paperContainer}>
+          <PrimarySearchAppBar setCity={this.setCity}/>
+          <div className="container">
+            <MediaCard city={this.state.city}/> 
+          </div>
+        </Paper>
+      </div>
+    );
+  }
 }
 
 export default App;
