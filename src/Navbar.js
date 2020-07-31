72278c986e566d77875818063300bc4c2d71b98d
unknown <sreejusivaram47@gmail.com>
2020-08-01 01:14:12 +0530

    Added autocomplete search

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,6 +15,9 @@ import InfoIcon from '@material-ui/icons/Info';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import { Tooltip } from '@material-ui/core';
 import CloudIcon from '@material-ui/icons/Cloud';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -58,7 +61,6 @@ const useStyles = makeStyles((theme) => ({
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
     width: '100%',
@@ -101,11 +103,6 @@ export default function PrimarySearchAppBar(props) {
   const handleMenuClose = () => {
     setAnchorEl(null);
     handleMobileMenuClose();
-  };
-
-  const handleSearch = () => {
-    console.log('inside handle search', value);
-    props.setCity(value)
   };
 
   const handleMobileMenuOpen = (event) => {
@@ -162,35 +159,59 @@ export default function PrimarySearchAppBar(props) {
     </Menu>
   );
 
+  const cities = ['Mumbai','Delhi','Kolkata','Chennai','Bangalore','Hyderabad','Ahmedabad','Pune','Surat','Jaipur','Kanpur','Lucknow','Nagpur',
+  'Ghaziabad','Indore','Coimbatore','Kochi','Patna','Kozhikode','Bhopal','Thrissur','Vadodara','Agra','Visakhapatnam','Malappuram','Thiruvananthapuram','Kannur','Ludhiana','Nashik','Vijayawada'
+    ,'Madurai','Varanasi','Meerut','Faridabad','Rajkot','Jamshedpur','Srinagar','Jabalpur','Asansol','Vasai-Virar'
+    ,'Allahabad','Dhanbad','Aurangabad','Amritsar','Jodhpur','Ranchi','Raipur','Kollam','Gwalior','Bhilai','Chandigarh','Tiruchirappalli','Kota','Mysore','Bareilly','Tiruppur','Guwahati',
+    'Solapur','Hubli-Dharwad','Salem','Aligarh','Gurugram','Moradabad','Bhubaneswar','Jalandhar','Warangal','Bhiwandi','Siliguri','Saharanpur','Gorakhpur',
+    'Guntur','Cuttack','Puducherry','Amravati','Bikaner','Noida','Mangalore','Belagavi','Bhavnagar','Firozabad','Jamnagar','Durgapur','Malegaon','Bokaro',
+    'Nellore','Kolhapur','Raurkela','Ajmer','Nanded','Jhansi',
+    'Gulbarga','Erode','Ujjain','Sangli','Tirunelveli','Muzaffarnagar','Vellore','Kurnool']
+
   return (
     <div className={classes.grow}>
       <AppBar position="static" color="transparent">
         <Toolbar>
-          <Typography className={classes.title} variant="h6" noWrap>
+          <Typography className="cloud-logo" variant="h6" noWrap>
             <CloudIcon/>
           </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-                <IconButton
-                    className={classes.iconButton}
-                    color="inherit" 
-                    aria-label="search"
-                    onClick={handleSearch}
-                >
-                    <SearchIcon />
-                </IconButton>
-            </div>
-            <InputBase
+          <div className="search-bar">
+            <Autocomplete
+            disableClearable
+            options={cities.map((option) => option)}
+            onChange={(event, value) => {
+                props.setCity(value)
+                setValue(value)
+            }}
+            renderInput={(params) => (
+          <TextField
+            classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+            }}
+            {...params}
+            placeholder="Search…"
+            margin="normal"
+            variant="outlined"
+            InputProps={{ 
+                ...params.InputProps,
+                startAdornment: <InputAdornment position="start"> <SearchIcon /></InputAdornment>,
+                type: 'search' }}
+          />
+        )}
+      />
+            {/* <InputBase
               placeholder="Search…"
               classes={{
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
               inputProps={{ 'aria-label': 'search' }}
+              onKeyDown={handleKeyDown}
               onChange={(event) => {
                     setValue(event.target.value)
                 }}
-            />
+            /> */}
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
